a07fcf499cdf4592b8521e56cbc8ad044488e1eb
dlehdwn <155503289+dlehdwn@users.noreply.github.com>
2024-01-04 12:51:13 +0900

    Add files via upload

---
+++ Day02/data_file.py
@@ -0,0 +1,15 @@
+# datatype
+num1 = int(input("첫번째 수:"))
+num2 = int(input("두번째 수:"))
+print(f"합: {num1 + num2}")
+print(f"차: {num1 - num2}")
+print(f"곱: {num1 * num2}")
+
+
+
+# input() :  뮤저한테 입력받는 기능
+# print() : 안에 들어있는 내용 출력
+# int() : 안에 들어있는 문자를 정수화
+
+# cpu [연산] + ram [기억]
+# = 대입연산자
